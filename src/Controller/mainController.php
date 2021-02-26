11f050168cb2f7862da77b42beb5b693ae3e5aed
Alireza <alirezaeiji151379@gmail.com>
2021-02-26 22:35:16 +0330

    باگ ریز

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -18,78 +18,73 @@
 //$requestedMethod=$_SERVER["REQUEST_METHOD"];
 //
 //
-//$controller=null;
-//if($uri[5]=="User"){
-//    if(!isset($uri[6])) $controller=new UserController($requestedMethod);
-//    else $controller=new UserController($requestedMethod,$uri[6]);
-//}
-//if($uri[5]=="Idea"){
-//    if(!isset($queries["type"])){
-//        if(isset($uri[6]))
-//        $controller=new IdeaController($requestedMethod,null,$uri[6]);
-//        else $controller=new IdeaController($requestedMethod);
-//
-//    }
-//    else{
-//        if($queries["type"]=="owner" && isset($uri[6]) ) $controller=new IdeaController($requestedMethod,$uri[6]);
-//        elseif($queries["type"]=="idea" && isset($uri[6])) $controller=new IdeaController($requestedMethod,null,$uri[6]);
-//    }
-//
-//}
-//if ($uri[5]=="Patent"){
-//    if(!isset($queries["type"])){
-//        if(isset($uri[6]))$controller=new PatentController($requestedMethod,$uri[6]);
-//        else $controller=new PatentController($requestedMethod);
-//    }
-//    else{
-//        if($queries["type"]=="owner" && isset($uri[6])) $controller=new PatentController($requestedMethod,null,$uri[6]);
-//        elseif($queries["type"]=="patent" && isset($uri[6])) $controller=new PatentController($requestedMethod,$uri[6],null);
-//    }
-//}
-//if($uri[5]=="auth"){
-//    $controller=new loginController($requestedMethod);
-//}
-//if($uri[5]=="refresh"){
-//    $controller=new authHandler("GET",null,"zdf");
-//}
-//if($uri[5]=="authUser"){
-//    $controller=new authHandler("GET","User",null);
-//}
-//if($uri[5]=="authAdmin"){
-//    $controller=new authHandler("GET","Admin",null);
-//}
-//if($uri[5]=="authAssistant"){
-//    $controller=new authHandler("GET","Assistant",null);
-//}
-//if(is_null($controller)) die("unproccessable request!");
-//$controller->processRequest();
 
-echo "1";
+
 require_once("PatentController.php");
-echo "2";
 require_once("UserController.php");
-echo "3";
 require_once("loginController.php");
-echo "4";
 require_once("../Model/User.php");
-echo "5";
 require_once("IdeaController.php");
-echo "6";
 header("Content-Type: application/json; charset=UTF-8");
-echo "7";
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-echo "8";
 header("Access-Control-Allow-Origin: *");
-echo "9";
 header("Access-Control-Max-Age: 3600");
-echo "10";
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-echo "11";
 parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $queries);
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 $requestedMethod = $_SERVER["REQUEST_METHOD"];
-print_r($uri);
+
+$controller=null;
+if($uri[5]=="User"){
+    if(!isset($uri[6])) $controller=new UserController($requestedMethod);
+    else $controller=new UserController($requestedMethod,$uri[6]);
+}
+if($uri[5]=="Idea"){
+    if(!isset($queries["type"])){
+        if(isset($uri[6]))
+        $controller=new IdeaController($requestedMethod,null,$uri[6]);
+        else $controller=new IdeaController($requestedMethod);
+
+    }
+    else{
+        if($queries["type"]=="owner" && isset($uri[6]) ) $controller=new IdeaController($requestedMethod,$uri[6]);
+        elseif($queries["type"]=="idea" && isset($uri[6])) $controller=new IdeaController($requestedMethod,null,$uri[6]);
+    }
+
+}
+if ($uri[5]=="Patent"){
+    if(!isset($queries["type"])){
+        if(isset($uri[6]))$controller=new PatentController($requestedMethod,$uri[6]);
+        else $controller=new PatentController($requestedMethod);
+    }
+    else{
+        if($queries["type"]=="owner" && isset($uri[6])) $controller=new PatentController($requestedMethod,null,$uri[6]);
+        elseif($queries["type"]=="patent" && isset($uri[6])) $controller=new PatentController($requestedMethod,$uri[6],null);
+    }
+}
+if($uri[5]=="auth"){
+    $controller=new loginController($requestedMethod);
+}
+if($uri[5]=="refresh"){
+    $controller=new authHandler("GET",null,"zdf");
+}
+if($uri[5]=="authUser"){
+    $controller=new authHandler("GET","User",null);
+}
+if($uri[5]=="authAdmin"){
+    $controller=new authHandler("GET","Admin",null);
+}
+if($uri[5]=="authAssistant"){
+    $controller=new authHandler("GET","Assistant",null);
+}
+if(is_null($controller)) die("unproccessable request!");
+$controller->processRequest();
+
+
+
+
+///last version
 //$user = loginController::sessionBasedLogin();
 //if ($user == false) {
 //    if ((($uri[5] == "User" || $uri[5] == "auth") && $requestedMethod == "POST") == false) {
